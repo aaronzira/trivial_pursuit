4fa2a7bb8fd79f6d17d9349f60a6c5461cb5a864
Aaron Zira <aaronzira@gmail.com>
2020-05-18 22:43:25 -0700

    Use manually created legend; tidying

--- trivial_pursuit.py
+++ trivial_pursuit.py
@@ -1,6 +1,7 @@
 import argparse
 from collections import defaultdict
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 from operator import itemgetter
 from random import randrange
@@ -20,13 +21,13 @@ players = [{'name': name_color.split('_')[0].upper(),
            for name_color in args.players_colors]
 
 # perimiter spaces
-p_coords = [(6, i*(2*np.pi/42)) for i in range(42)]
+p_coords = [(6, i * (2 * np.pi / 42)) for i in range(42)]
 p_coords.append((0, 0))
 # will plot 'wedge' spaces a second time with larger markers
-w_coords = [(6, i*(2*np.pi/42)) for i in range(42) if i % 7 == 0]
+w_coords = [(6, i * (2 * np.pi / 42)) for i in range(42) if i % 7 == 0]
 
 # 'spoke' spaces
-s_coords = [(k, j*(2*np.pi/42)) for k in range(1, 6) for j in range(0, 42, 7)]
+s_coords = [(k, j * (2 * np.pi / 42)) for k in range(1, 6) for j in range(0, 42, 7)]
 
 # coordinates for plotting
 p_r, p_th = zip(*p_coords)
@@ -41,27 +42,29 @@ s_r, s_th = zip(*s_coords)
 s_r = np.asarray(s_r)
 s_th = np.asarray(s_th)
 
-color_names = ['brown', 'green', 'magenta', 'yellow', 'orange', 'blue']
+color_names = [('brown', 'SCIENCE & NATURE'), ('green', 'SPORTS & LEISURE'),
+               ('magenta', 'ARTS & ENTERTAINMENT'), ('yellow', 'HISTORY'),
+               ('orange', 'WILD CARD'), ('blue', 'PEOPLE & PLACES')]
 spoke_colors = []
 # calculate colors of spoke spaces based on observed pattern and plotting order
 for i in [4, 2, 1, 5, 0]:
-    c_temp = color_names[i:]+color_names[:i]
-    spoke_colors.extend(c_temp)
+    c_temp = color_names[i:] + color_names[:i]
+    spoke_colors.extend(col for col, _ in c_temp)
 
 # calculate and keep track of locations/indices of perimiter spaces
 d = {}
 idxs = set()
 order = [1, 10, 21, 32, 41]
-for i, col in enumerate(color_names):
+for i, (col, _) in enumerate(color_names):
     d[col] = []
     places = order[i:] + order[:i]
     for val in places:
-        idx = (val+(i*7))%42
+        idx = (val + (i * 7)) % 42
         d[col].append(idx)
         idxs.add(idx)
 
 # wedge coordinates
-wedge_dict = {([p_th[idx] for idx in idxs if idx % 7 == 0][0], 6): col[:2]
+wedge_dict = {([p_th[idx] for idx in idxs if idx % 7 == 0][0], 6): col
               for col, idxs in d.items()}
 
 # add 'roll again' spaces
@@ -72,7 +75,7 @@ for val in range(42):
 d['k'] = 42
 
 # indices of just wedge spaces, for plotting with a larger markersize
-wedge_d = {col: i for i, col in enumerate(color_names[3:] + color_names[:3])}
+wedge_d = {col: i for i, (col, _) in enumerate(color_names[3:] + color_names[:3])}
 
 # indices of spaces on spokes
 spoke_d = defaultdict(list)
@@ -82,6 +85,11 @@ for i, col in enumerate(spoke_colors):
 thetas = np.concatenate([p_th, s_th])
 rs = np.concatenate([p_r, s_r])
 lines = []
+
+# for 'manual' legend and key to topics
+fig, ax = plt.subplots(1, 2, figsize=(13, 9))
+ax[0].set_axis_off()
+ax[1].set_axis_off()
 
 for col, idxs in d.items():
     line = plt.polar(p_th[idxs], p_r[idxs], 'H', markeredgecolor='k', picker=5,
@@ -104,26 +112,26 @@ plt.gca().title.set_weight('bold')
 lines = np.asarray(sorted([v for l in lines for v in l]))
 
 # drop the tokens on the board, mostly away from spaces
-markers = [plt.polar([.6],[i], marker='X', color=d['color'], markersize=18,
+markers = [plt.polar([.6], [i], marker='X', color=d['color'], markersize=18,
                      lw=0, markeredgewidth=1.5, markeredgecolor='w',
                      label=d['name'])[0]
             for i, d in enumerate(players)]
 
-# names/colors/current turn legend with wedge tracker
-legend = plt.legend(loc='upper left', prop={'weight': 'bold', 'size': 18},
-                    bbox_to_anchor=(1, 1), markerscale=0, handlelength=0,
-                    handletextpad=0, frameon=False)
-for i, l in enumerate(legend.get_texts()):
-    l.set_color(players[i]['color'])
-    l.set_text(f'{players[i]["name"]} -')
-token_idx = -1
+# explanatory table
+#table = mpatches.Rectangle((-.31, .74), .61, .24, fill=True, color='darkgray')
+#table.set_clip_on(False)
+#ax[0].add_patch(table)
+for i, (col, topic) in enumerate(color_names):
+    #ax[0].text(-.3, .2 - (i / 25), topic, color=col, weight='bold', fontsize=14)
+    ax[1].text(.6, .2 - (i / 25), topic, color=col, weight='bold', fontsize=14)
 
-def refresh(legend_obj):
-    '''Update a legend with each player's name and wedge tally'''
-    for i, l in enumerate(legend_obj.get_texts()):
-        l.set_text((f'{players[i]["name"]} - '
-                    f'{"|".join(sorted(players[i]["wedges"]))}'))
-    return
+# names/colors/current turn legend with wedge tracker
+for idx, player in enumerate(players):
+    ax[1].text(.6, .99 - (idx + 1) / 22, player['name'],
+                 weight='bold',
+                 color=player['color'],
+                 fontsize=18)
+token_idx = -1
 
 def get_closest(mx, my):
     ''''Find the closest space on the board to the location of given
@@ -132,16 +140,16 @@ def get_closest(mx, my):
     # NOTE: if the click is close to theta=0 but below the line, i.e. slightly
     # less than 2pi, the wrong space will be chosen
     mouse = np.asarray([mx, my])
-    dist_2 = np.sum((lines - mouse)**2, axis=1)
+    dist_2 = np.sum((lines - mouse) ** 2, axis=1)
     idx = np.argmin(dist_2)
     return lines[idx]
 
 def on_key(event):
     global ud
     global token_idx
-    global legend
     global mk_idx
     global current_tag
+    global marker
 
     # only care about SPACE, 'd', and 'w'
     if not event.key in ' wd':
@@ -165,9 +173,16 @@ def on_key(event):
         except KeyError:
             return
         # update this player as having collected this wedge
+        if wedge_color in players[token_idx]['wedges']:
+            return
+        wedge = mpatches.Rectangle((.78 + (len(players[token_idx]['wedges']) * .035),
+                                   .992 - (token_idx + 1) / 22),
+                                   .035, .025,
+                                   fill=True,
+                                   color=wedge_color)
+        ax[1].add_patch(wedge)
         players[token_idx]['wedges'].add(wedge_color)
-        refresh(legend)
-        event.canvas.draw_idle()
+        event.canvas.draw()
         return
 
     # 'd' -- roll the die
@@ -182,24 +197,25 @@ def on_key(event):
 
     # SPACE -- start next player's turn
     # indicate end of previous player's turn in legend
-    players[token_idx]['name'] = players[token_idx]['name'].lstrip('*')
+    try:
+        marker.remove()
+    except NameError:
+        pass
     token_idx += 1
     if token_idx == len(players):
         token_idx = 0
     # indicate this player's turn in legend
-    players[token_idx]['name'] = '***' + players[token_idx]['name']
-    refresh(legend)
-    print(f'Team {players[token_idx]["name"].lstrip("*")} to move')
-    event.canvas.flush_events()
+    marker = ax[1].text(.57, .984 - (token_idx + 1) / 22, '*',
+                          weight='bold',
+                          color=players[token_idx]['color'],
+                          fontsize=18)
+    print(f'Team {players[token_idx]["name"]} to move')
     x, y = markers[token_idx].get_xydata()[0]
     label = markers[token_idx].get_label()
     # indicate location of current token before first roll
     current_tag = plt.annotate(label, xy=(x, y), weight='bold', color='k',
                                fontsize=12, bbox=dict(boxstyle='round,pad=0.2',
                                fc='w', alpha=0.7))
-    # alternate 'to move' indicator instead of using the main legend
-    #legend2 = plt.legend(handles=[markers[token_idx]], loc='upper left',
-                          #bbox_to_anchor=(0,1), labels=['TO MOVE'])
     event.canvas.draw()
     mk_idx = markers[token_idx]
     # dissociate movement with previous player
@@ -214,9 +230,9 @@ def on_key(event):
         if event.mouseevent.button != 1:
             return
         x, y = get_closest(event.mouseevent.xdata, event.mouseevent.ydata)
-        if np.abs((np.pi*2) - x) < .01:
+        if np.abs((np.pi * 2) - x) < .01:
             x = 0
-        mk_idx.set_data([x],[y])
+        mk_idx.set_data([x], [y])
         event.canvas.draw_idle()
     ud = plt.gcf().canvas.mpl_connect('pick_event', update)
     return
